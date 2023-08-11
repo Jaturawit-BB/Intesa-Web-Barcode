b60fe6066f97b4dd65a27fb3ad7d0bc05f581687
Jaturawit-BB <139746649+Jaturawit-BB@users.noreply.github.com>
2023-08-11 14:02:55 +0700

    Add files via upload

--- src/Component/Page/Navbar.js
+++ src/Component/Page/Navbar.js
@@ -3,22 +3,33 @@ import * as FaIcons from "react-icons/fa";
 import * as AiIcons from "react-icons/ai";
 import { Link } from "react-router-dom";
 import { SidebarData } from "./SlideBar";
-import "../../App.css"
-// import "../App.css";
+import "../../App.css";
 import { IconContext } from "react-icons";
+import IntesaLogo from "../../Icon/IntesaLogo.png";
 
 function Navbar() {
   const [sidebar, setSidebar] = useState(false);
+  // const [currentMenu, setCurrentMenu] = useState("Home"); // Add a state variable to store the current menu name
 
   const showSidebar = () => setSidebar(!sidebar);
+
+  // const handleMenuItemClick = (title) => {
+  //   setCurrentMenu(title); 
+  //   showSidebar(); 
+  // };
 
   return (
     <>
       <IconContext.Provider value={{ color: "undefined" }}>
         <div className="navbar">
-          <Link to="#" className="menu-bars">
+          <Link to="#" className={sidebar ? "menu-bars active" : "menu-bars"}>
             <FaIcons.FaBars onClick={showSidebar} />
+            <img src={IntesaLogo} alt="" style={{ width: 250, height: 80 }} />
           </Link>
+          {/* <div className="menu-name">
+            <img src={IntesaLogo} alt="" />
+          </div>{" "} */}
+          {/* Display the current menu name */}
         </div>
         <nav className={sidebar ? "nav-menu active" : "nav-menu"}>
           <ul className="nav-menu-items" onClick={showSidebar}>
@@ -30,7 +41,12 @@ function Navbar() {
             {SidebarData.map((item, index) => {
               return (
                 <li key={index} className={item.cName}>
-                  <Link to={item.path}>
+                  <Link
+                    to={item.path}
+                    // onClick={() => handleMenuItemClick(item.title)}
+                  >
+                    {" "}
+                    {/* Call handleMenuItemClick when a menu item is clicked */}
                     {item.icon}
                     <span>{item.title}</span>
                   </Link>
